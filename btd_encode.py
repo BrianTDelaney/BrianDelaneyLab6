b2d56fd90bb674613671ffe1f68589d7bd587254
BrianTDelaney <brian.delaney@ufl.edu>
2023-10-25 20:05:09 -0400

    Fixed variable types in encode method and whitespace in main method.

--- btd_encode.py
+++ btd_encode.py
@@ -3,6 +3,6 @@
 def encode(number):
     encoded_number = ''
     for digit in number:
-        digit = int(digit) + 3
+        digit = str(int(digit) + 3)
         encoded_number = encoded_number + digit
     return encoded_number
--- btd_main.py
+++ btd_main.py
@@ -1,21 +1,21 @@
 def main():
     while True:
         print('''Main Menu
-        ---------
-        1. Encode
-        2. Decode
-        3. Exit\n''')
+---------
+1. Encode
+2. Decode
+3. Exit\n''')
         choice = int(input('Please select an option from the menu: '))
         if choice == 1:
             password = input('\nPlease enter the password you would like to encode: ')
             encoded_password = encode(password)
-            print(encoded_password)
+            print(f'\nYour encoded password is: {encoded_password}\n')
         elif choice == 2:
             encoded_password = input('\nPlease enter the encoded password you would like to decode: ')
             password = decode(encoded_password)
-            print(password)
+            print(f'\nYour decoded password is: {password}\n')
         elif choice == 3:
-            print('Goodbye!')
+            print('\nGoodbye!')
             break
 
 
